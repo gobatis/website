8b4232dca293b60ca7b99c96c04a8431ad25445e
koyeo <koyeo@qq.com>
2023-09-11 13:04:55 +0800

    update doc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'Gobatis',
+    title: 'Gobatis【developing】',
     tagline: 'Make system building simple and efficient',
     favicon: 'img/favicon.ico',
 
